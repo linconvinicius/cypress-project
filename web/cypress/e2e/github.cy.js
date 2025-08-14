2ff4c0ebbdc76b4e91735cf2c142c6ec61a405e5
linconvinicius <lvini_@live.com>
2025-08-14 16:06:03 -0300

    feat: implementando test de acesso ao GH

--- web/cypress/e2e/github.cy.js
+++ web/cypress/e2e/github.cy.js
@@ -4,7 +4,7 @@ describe('Gerenciamento de perfis no GitHub', () => {
         cy.goTo("Tabela", "Perfis do GitHub");
     })
 
-    it('Deve poder cadastrar um novo perfil dp github', () => {
+    it('Deve poder cadastrar um novo perfil do github', () => {
         cy.get('#name').type('Lincon Vinicius');
         cy.get('#username').type('linconvinicius');
         cy.get('#profile').type('QA')
@@ -30,7 +30,56 @@ describe('Gerenciamento de perfis no GitHub', () => {
             .should('be.visible')
 
         //cy.contains('title', 'Abrir perfil no GitHub')
-            //.should('have.attr', 'href', 'https://github.com/linconvinicius')
-            //.and('have.attr', 'target', '_blank')
+        //.should('have.attr', 'href', 'https://github.com/linconvinicius')
+        //.and('have.attr', 'target', '_blank')
+    })
+
+    it('Deve poder remover um perfil do github', () => {
+
+        const profile = {
+            name: 'Lincon Vinicius',
+            username: 'felipefr',
+            description: 'QA'
+        }
+
+        cy.get('#name').type(profile.name);
+        cy.get('#username').type(profile.username);
+        cy.get('#profile').type(profile.description)
+
+        cy.contains('button', 'Adicionar Perfil').click();
+
+        cy.contains('table tbody tr', profile.username)
+            .should('be.visible')
+            .as('trProfile')
+
+        cy.get('@trProfile')
+            .find('button[title="Remover perfil"]')
+            .click();
+
+        cy.contains('table tbody tr', profile.username)
+            .should('not.exist')
+    })
+
+    it.only('Deve validar o link do github', () => {
+
+        const profile = {
+            name: 'Lincon Vinicius',
+            username: 'linconvinicius',
+            description: 'QA'
+        }
+
+        cy.get('#name').type(profile.name);
+        cy.get('#username').type(profile.username);
+        cy.get('#profile').type(profile.description)
+
+        cy.contains('button', 'Adicionar Perfil').click();
+
+        cy.contains('table tbody tr', profile.username)
+            .should('be.visible')
+            .as('trProfile')
+
+        cy.get('@trProfile')
+            .find('a')
+            .should('have.attr', 'href', 'https://github.com/linconvinicius')
     })
 })
